4024c9c063afe28c6dd7fb2f47adff7b60a820a8
Emmanuel Owusu Yeboah <123456789+odin@users.noreply.github.com>
2024-07-06 07:05:51 +0000

    remove theme js code

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 "use strict";
+const calcContainer = document.querySelector(".calc-container");
+const calcText = document.querySelector(".calc-text");
+const calcBtn = document.querySelector(".calc-btn");
+
+const caldBtns = document.querySelectorAll(".btn");
+const calcScreen = document.querySelector(".calc-screen");
+
+const evalScreen = document.querySelector(".calc-eval");
+const numbers = document.querySelectorAll(".num");
+
+let firstNum = "";
+let operand = "";
+let secondNum = "";
+let origin = 0;
